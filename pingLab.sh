fb3cecfb94e239f36ec4c17f7c480aca6980ad37
glebik000 <glebik000@gmail.com>
2020-05-09 00:45:07 +1000

    Not tested

--- pingLab.sh
+++ pingLab.sh
@@ -13,8 +13,7 @@
 	j=0
 	cnt=0
 	mod=0
-	zero=0
-	
+	zero=0	
 
 echo -e $CLEAR
 #echo c01 c02 c03 c04 c05 c06 c07 c08 c09 c10 c11 c12 c13 c14 bkv
@@ -46,6 +45,9 @@ do
 	ping -c 2 $j 2> /dev/null
 	if (($? == 0)); then
 		answ[$cnt]=$ON
+		ssh-copy-id -o "StrictHostKeyChecking no" $j
+		scp $j:/etc/passwd $j/
+		ssh $j 'ls -d -l /home/*' >$j/home.txt
 		else answ[$cnt]=$OFF
 	fi
 	let cnt++
